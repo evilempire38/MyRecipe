b00cdfd6ca66c65edf42daf2093e4ec8d144a054
evilempire38 <evilempire38@gmail.com>
2022-02-17 12:17:39 +0300

    initial commit

---
+++ MyRecipe/Controllers/FavoriteScene/FavouriteTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FavouriteTableViewController.swift
+//  MyRecipe
+//
+//  Created by Андрей Коноплёв on 17.02.2022.
+//
+
+import UIKit
+
+class FavouriteTableViewController: UITableViewController {
+    var favouriteRecipe : [Recipe] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return favouriteRecipe.count
+    }
+
+
+
+}
