165ea50ab251f7817a393d77b6c383e36ba64d0a
evilempire38 <evilempire38@gmail.com>
2022-02-23 16:47:33 +0300

    COREDATA COMMIT
    FINALY ADD THIS ONE

--- MyRecipe/Controllers/FavoriteScene/FavouriteTableViewController.swift
+++ MyRecipe/Controllers/FavoriteScene/FavouriteTableViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreData
+
 
 class FavouriteTableViewController: UITableViewController {
     var favouriteRecipe : [MyFavouriteRecipes] = []
@@ -14,13 +16,33 @@ class FavouriteTableViewController: UITableViewController {
         
         super.viewDidLoad()
         self.title = "Favourites"
+        loadMyFavouriteRecipes()
+        
+
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadMyFavouriteRecipes()
+        self.tableView.reloadData()
+    }
+    private func loadMyFavouriteRecipes () {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let request = NSFetchRequest<MyFavouriteRecipes>(entityName: "MyFavouriteRecipes")
+        let context = appDelegate.coreDataStack.persistentContainer.viewContext
+        
+        do {
+            favouriteRecipe = try context.fetch(request)
+            print(favouriteRecipe)
+        }
+        catch  let error {
+            print(error)
+        }
 
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-
         return 1
     }
 
@@ -29,6 +51,19 @@ class FavouriteTableViewController: UITableViewController {
         return favouriteRecipe.count
     }
 
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favouriteCell", for: indexPath) as! RecipeListTableViewCell
+        let object = favouriteRecipe[indexPath.row]
+        if let dataImage = object.image {
+            guard let image : UIImage = UIImage(data: dataImage) else {return UITableViewCell()}
+            cell.reciepeImage.image = image
+        }
+        cell.recipeTitle.text = object.title
+        cell.recipeHealthScore.text = object.healthScore.description
+        cell.recipeTimeToReady.text = object.readyInMinutes.description
+        
+        
+        return cell
+    }
 
 }
