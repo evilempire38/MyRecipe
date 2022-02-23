087f2bbae02810a30e1c86aafce5612054edfc73
evilempire38 <evilempire38@gmail.com>
2022-02-23 13:40:03 +0300

    commit 1
    added 5 screen with faq and theme switcher

---
+++ MyRecipe/Models/InfoModel.swift
@@ -0,0 +1,13 @@
+//
+//  InfoModel.swift
+//  MyRecipe
+//
+//  Created by Андрей Коноплёв on 23.02.2022.
+//
+
+import Foundation
+
+struct InfoModel {
+    let typeOfsettings : String
+    
+}
---
+++ MyRecipe/Views/FAQTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FAQTableViewCell.swift
+//  MyRecipe
+//
+//  Created by Андрей Коноплёв on 23.02.2022.
+//
+
+import UIKit
+
+class FAQTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
